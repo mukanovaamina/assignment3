13c62e1dc0dd51a7c820912252ba95982127b6a2
Admin <221250@astanait.edu.kz>
2023-04-29 03:16:37 +0600

    I created a new class named MyArrayListStack for the MyArrayList implementation.

---
+++ MyArrayListStack.java
@@ -0,0 +1,9 @@
+import java.util.EmptyStackException;
+
+public class MyArrayListStack<E> {
+    private MyArrayList<E> list;
+
+    public MyArrayListStack() {
+        list = new MyArrayList<>();
+    }
+}
