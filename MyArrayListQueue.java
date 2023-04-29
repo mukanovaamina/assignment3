e68b73ab3648379adb0b17148909a5eb5c8781c7
Admin <221250@astanait.edu.kz>
2023-04-29 18:13:59 +0600

    Implemented the enqueue method that adds an element to the back of the queue using the add method for MyArrayList.

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -6,5 +6,8 @@ public class MyArrayListQueue<E> {
     public MyArrayListQueue() {
         list = new MyArrayList<>(); //instance variable
     }
+    public void enqueue(E element) { //method is to add an element to the back of the queue
+        list.add(element);
+    }
 
 }
