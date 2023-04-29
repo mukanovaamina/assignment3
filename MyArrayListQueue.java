485321425dd9a2d7749bb6180a6b01eb1bc265be
Admin <221250@astanait.edu.kz>
2023-04-29 18:01:59 +0600

    Imported the necessary classes: java.util.NoSuchElementException for the queue exceptions. Declared an instance variable of type ArrayList inside the class.

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -1,6 +1,10 @@
-import java.util.NoSuchElementException;
+import java.util.NoSuchElementException; //necessary exception
 
 public class MyArrayListQueue<E> {
+    private MyArrayList<E> list;
 
+    public MyArrayListQueue() {
+        list = new MyArrayList<>(); //instance variable
+    }
 
 }
