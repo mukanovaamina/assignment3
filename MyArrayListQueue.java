3c9bb7f72c1822f1f5fd7dc3a02766f3e7370e5a
Admin <221250@astanait.edu.kz>
2023-04-29 18:42:36 +0600

    Implemented the peek method that returns the front element of the queue without removing it using the get method for MyArrayList.

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -15,5 +15,11 @@ public class MyArrayListQueue<E> {
         }
         return list.remove(0);
     }
+    public E peek() { // method is to return the front element of the queue without removing it
+        if (list.isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return list.get(0);
+    }
 
 }
